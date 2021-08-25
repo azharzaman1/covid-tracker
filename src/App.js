bf92916c63d06409b34e898f23fb978677a1b577
Azhar Zaman <68331974+TheAzharZaman@users.noreply.github.com>
2021-08-26 01:09:09 +0500

    Responsiveness almost Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Homepage from "./Pages/Homepage";
+import Authentication from "./Pages/Authentication";
+import Login from "./Pages/Login";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { useStateValue } from "./Files/StateProvider";
+import { auth, db } from "./Files/firebase";
 
-function App() {
+const App = () => {
+  const [{ currentUser }, dispatch] = useStateValue();
+  const [fetchedData, setFetchedData] = useState();
+  const [secureData, setSecureData] = useState({});
+
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    console.log("CURRENT LOGGED IN USER ->>>>", currentUser);
+  }, [currentUser]);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        setUser(authUser);
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+        localStorage.setItem("userID", authUser?.uid);
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    const fetchDataFromDB = () => {
+      const docRef = db.collection("users").doc(user?.uid);
+
+      docRef.get().then((doc) => {
+        setFetchedData(doc.data());
+        dispatch({
+          type: "SET_FETCHED_DETAILS",
+          fetchedData: doc.data(),
+        });
+      });
+    };
+
+    fetchDataFromDB();
+  }, [user]);
+
+  useEffect(() => {
+    setSecureData({
+      displayName: fetchedData?.displayName,
+      userID: fetchedData?.userID,
+      email: fetchedData?.email,
+    });
+  }, [fetchedData]);
+
+  useEffect(() => {
+    localStorage.setItem("fetchedData", JSON.stringify(secureData));
+  }, [secureData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {currentUser && <Redirect to="/" />}
+      {!currentUser && <Redirect to="/auth" />}
+
+      <div className="app__cont">
+        <Switch>
+          <Route path="/auth/login">
+            <Login />
+          </Route>
+          <Route path="/auth">
+            <Authentication />
+          </Route>
+          <Route path="/">
+            <Homepage displayName={fetchedData?.displayName} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import { StateProvider } from "./Files/StateProvider";
+import reducer, { initialState } from "./Files/reducer";
+import * as serviceWorker from "./serviceWorker";
+
+let RootDirectory = document.getElementById("root");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  RootDirectory
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.register();
